adae0b8827426b57ec7091aac0167fb2727dc8a4
Eduard Khabisov <36218165+volchonokboy@users.noreply.github.com>
2020-11-24 00:55:37 +0500

    Fix output

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ for el in events:
     try:
         y = event['event']
     except:
-        err = f'File: **{el}**\n\n'
+        err = '---\n'
+        err += f'File: **{el}**\n\n'
         print(err + f'Error: empty dict\n')
         y = False
 
@@ -43,18 +44,21 @@ for el in events:
             with open('./schema/workout_created.schema') as json_file:
                 schema = json.load(json_file)
         else:
-            err = f'File: **{el}**, schema: **{event["event"]}**\n\n'
+            err = '---\n'
+            err += f'File: **{el}**, schema: **{event["event"]}**\n\n'
             print(err + f'Error: "{event["event"]}" don\'t find schema in folder "schema"\n\n')
             schema = False
     else:
-        err = f'File: **{el}**\n\n'
+        err = '---\n'
+        err += f'File: **{el}**\n\n'
         print(err + f'Error: different file type\n\n')
         schema = False
 
     if schema:
         v = Draft7Validator(schema)
         errors = sorted(v.iter_errors(event['data']), key=lambda e: e.path)
-        err = f'File: **{el}**, schema: **{event_name}**\n\n'
+        err = '---\n'
+        err += f'File: **{el}**, schema: **{event_name}**\n\n'
         for error in errors:
             err += f'Path: {list(error.absolute_schema_path)}, Error: {error.message}\n\n'
             print(err)
